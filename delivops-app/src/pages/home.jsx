e212fab0261ebd06c28a956e8c3c126aa5761729
Lea Gomis <lea.187@hotmail.fr>
2025-08-05 23:23:32 +0200

    feat(auth): ajout des boutons de connexion et déconnexion dans la page Home

--- delivops-app/src/pages/home.jsx
+++ delivops-app/src/pages/home.jsx
@@ -1,13 +1,34 @@
 import { useLocation } from 'react-router-dom';
 import DeliveryFormGroup from '../components/DeliveryFormGroup';
+import { useAuth0 } from '@auth0/auth0-react';
+
 
 export default function Home() {
   const location = useLocation();
   const importedTours = location.state?.tours;
 
+  const { loginWithRedirect, logout, isAuthenticated, user } = useAuth0();
+
   return (
     <div>
       <h1>Accueil</h1>
+      
+      {/* Zone d'authentification */}
+      <div>
+        {!isAuthenticated && (
+          <button onClick={() => loginWithRedirect()}>Se connecter</button>
+        )}
+
+        {isAuthenticated && (
+          <div>
+            <p>Connecté en tant que : {user.name}</p>
+            <button onClick={() => logout({ returnTo: window.location.origin })}>
+              Se déconnecter
+            </button>
+          </div>
+        )}
+      </div>
+
       <DeliveryFormGroup initialData={importedTours} />
     </div>
   );
